110267c7027a97f875b822899924b223f6d790b4
Alex Ellis <alexellis2@gmail.com>
2017-09-24 20:41:52 +0100

    Apply redis cache
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- tweet-fanclub/handler.py
+++ tweet-fanclub/handler.py
@@ -1,10 +1,34 @@
 import requests
 import json
 import base64
+import redis
+import os
 
 def handle(st):
     # parse Github event
     req = json.loads(st)
+
+    minutes = 1
+    minutes_val = os.getenv("cache-minutes")
+    if minutes_val != None:
+        minutes = int(minutes_val)
+
+    loginName = req["sender"]["login"]
+
+    try:
+        redis_client = redis.StrictRedis("redis")
+        redis_key = "tweet-" + loginName
+
+        cached = redis_client.get(redis_key)
+
+        if cached != None:
+            print(loginName + " attempted to trigger event again before cache expired. Extending cache timeout.")
+            redis_client.setex(redis_key, 60 * minutes, "1")
+            return
+
+        redis_client.setex(redis_key, 60 * minutes, "1")
+    except Exception:
+        print("Redis may be down or errored")
 
     # download the avatar binary using getavatar function
     r = requests.post("http://gateway:8080/function/get-avatar", json=req)
@@ -18,7 +42,6 @@ def handle(st):
 
     # Take the encoded image and turn into binary bytes
     imageData = base64.standard_b64decode(res["content"])
-    loginName = req["sender"]["login"]
     put_url = "http://minio-shim:8080/put/" + loginName + ext
     # Store in the fan-club photo gallery
     r1 = requests.post(put_url, data= imageData)
